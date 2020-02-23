f49137dcfb68796f176f22035c0f6fe6d28bc869
Vitali Karpuk <karpuk-job@yandex.ru>
2020-02-23 13:31:40 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined || array.length < 1) return 0
+  let min = array.sort((a,b) => a-b);
+  
+  return min[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined || array.length < 1) return 0
+  let max = array.sort((a,b) => b-a)
+  return max[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined || array.length < 1) return 0
+  let length = array.length;
+  let sum = array.reduce((acc, item) => acc + item , 0)
+  return sum/length;
 }
